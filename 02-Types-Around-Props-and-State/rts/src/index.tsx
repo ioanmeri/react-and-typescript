2f9c582afbb44a4c593bacebc957c10aac7a8f42
Ioannis Merianos <ioannismeri@gmail.com>
2022-04-19 23:13:28 +0300

    types around refs

--- 02-Types-Around-Props-and-State/rts/src/index.tsx
+++ 02-Types-Around-Props-and-State/rts/src/index.tsx
@@ -1,10 +1,11 @@
 import ReactDOM from 'react-dom';
+import UserSearch from './refs/UserSearch'
 import EventComponent from './events/EventComponent';
 
 const App = () => {
   return (
     <div>
-      <EventComponent />
+      <UserSearch />
     </div>
   )
 };
